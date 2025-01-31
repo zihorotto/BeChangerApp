8e5976f2a08754242b632c9f384323dd7fd28f01
Otto Zihor <zihorotto@gmail.com>
2025-01-31 13:21:55 +0100

    IGEN!!

--- backend/src/main/java/com/bechanger/auth/AuthenticationRequest.java
+++ backend/src/main/java/com/bechanger/auth/AuthenticationRequest.java
@@ -13,7 +13,6 @@ import lombok.Setter;
 @Builder
 public class AuthenticationRequest {
 
-
     @NotEmpty(message = "Email is mandatory")
     @NotBlank(message = "Email is mandatory")
     @Email(message = "Email is not formatted")
